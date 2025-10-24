6d815321547af9a764dffe3e6eb2e28ee651c454
Nathan Alegria <nathanalegria02@gmail.com>
2025-10-24 08:44:24 -0600

    Clase EmpleadoNormal

--- LabSemana2/src/labsemana2/EmpleadoNormal.java
+++ LabSemana2/src/labsemana2/EmpleadoNormal.java
@@ -4,10 +4,42 @@
  */
 package labsemana2;
 
+import java.util.Calendar;
+
 /**
  *
  * @author Nathan
  */
 public class EmpleadoNormal {
+
+    protected int codigo;
+    protected String nombre;
+    protected Calendar fechaContratacion;
+    protected double salarioBase;
+    protected int horasTrabajadas;
+
+    public EmpleadoNormal(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        fechaContratacion = Calendar.getInstance();
+        salarioBase = 1300;
+        horasTrabajadas = 0;
+    }
+
+    public void registrarHoras(int horas) {
+        horasTrabajadas = horas;
+    }
+
+    public double calcularPago() {
+        if (horasTrabajadas < 160) {
+            return ((salarioBase * horasTrabajadas) - (salarioBase * 0.35));
+        }
+        return ((salarioBase * 160) - (salarioBase * 0.35));
+    }
     
+    public String toString(){
+        return "Código: "+codigo
+                +"\nNombre: "+nombre
+                +"\nFecha de Contratación: "+fechaContratacion.getTime();
+    }
 }
